676b4564c5c3cf087fe66438860ba89f9a79015b
Mateus Brites <mateusmmo15@gmail.com>
2021-07-07 12:47:39 -0300

    subindo codigo para tirar duvidas com instrutor

---
+++ Chapter1/PrimeiroProjetoComNode/README.md
@@ -0,0 +1,3 @@
+# Criação das primeiras rotas e uso de Middleware
+## Nesse projeto nós criamos as primeiras rotas da aplicação e criamos um 
+## Middleware de autenticação de usuário. 
--- Chapter1/PrimeiroProjetoComNode/src/index.js
+++ Chapter1/PrimeiroProjetoComNode/src/index.js
@@ -7,39 +7,73 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistAccountCPF(request, response, next){
+    const { cpf } = request.headers;
+
+    const customer = customers.find( (customer) => customer.cpf === cpf);
+
+    if(!customer){
+        console.log(cpf);
+        return response.status(400).json({ error: "CPF not found" });
+    }
+
+    request.customer = customer;
+
+    return next();
+}
+
 app.post("/account", (request, response) => {
-    const { CPF, name } = request.body;
+    const { cpf, name } = request.body;
+    console.log(cpf);
 
 
     const custumerAlreadyExist = customers.some(
-        (custumer) => custumer.CPF === CPF
+        (custumer) => custumer.cpf === cpf
     );
 
     if(custumerAlreadyExist){
-        return response.status(400).json({ error: "CPF already exist"});
+        return response.status(400).json({ error: "cpf already exist"});
     }
 
     customers.push({
-        CPF,
+        cpf,
         name,
         id: uuidV4(),
         statement: []
     });
 
     return response.status(201).send();
-})
+});
 
-app.get("/statement", (request, response) => {
-    const { cpf: CPF } = request.headers;
-
-    const customer = customers.find( (customer) => customer.CPF === CPF);
-
-    if(!customer){
-        console.log(CPF);
-        return response.status(400).json({ error: "CPF not found" });
-    }
+app.get("/statement", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
 
     return response.json(customer.statement);
-})
+});
+
+app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
+
+    const { description, amount } = request.body;
+    const { customer } = request;
+    console.log(description, amount, request.body.body);
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).json(statementOperation);
+});
+
+app.post("/teste", verifyIfExistAccountCPF, (request, response) => {
+    const { teste } = request.body;
+    console.log(teste);
+
+    return response.status(201).send();
+});
 
 app.listen(3333, () => console.log("server started on port 3333"));
