810743ef7ea47fada5d02451f870df4f0b31af91
Mateus Brites <mateusmmo15@gmail.com>
2021-07-08 23:53:41 -0300

    fim do primeiro projeto

--- Chapter1/PrimeiroProjetoComNode/README.md
+++ Chapter1/PrimeiroProjetoComNode/README.md
@@ -1,3 +1,7 @@
 # Criação das primeiras rotas e uso de Middleware
 ## Nesse projeto nós criamos as primeiras rotas da aplicação e criamos um 
 ## Middleware de autenticação de usuário. 
+---
+<h1>
+    <img src="https://ik.imagekit.io/mbrites/Captura_de_tela_de_2021-07-08_23-47-37_oxCevpqt4.png">
+</h1>
--- Chapter1/PrimeiroProjetoComNode/src/index.js
+++ Chapter1/PrimeiroProjetoComNode/src/index.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const express = require("express");
 const { v4: uuidV4 } = require("uuid")
  
@@ -7,13 +8,24 @@ app.use(express.json());
 
 const customers = [];
 
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === "credit"){
+            return acc + operation.amount
+        } else {
+            return acc - operation.amount
+        }
+    }, 0);
+
+    return balance;
+}
+
 function verifyIfExistAccountCPF(request, response, next){
     const { cpf } = request.headers;
 
     const customer = customers.find( (customer) => customer.cpf === cpf);
 
     if(!customer){
-        console.log(cpf);
         return response.status(400).json({ error: "CPF not found" });
     }
 
@@ -24,8 +36,6 @@ function verifyIfExistAccountCPF(request, response, next){
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
-    console.log(cpf);
-
 
     const custumerAlreadyExist = customers.some(
         (custumer) => custumer.cpf === cpf
@@ -55,7 +65,6 @@ app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
 
     const { description, amount } = request.body;
     const { customer } = request;
-    console.log(description, amount, request.body.body);
 
     const statementOperation = {
         description,
@@ -69,11 +78,72 @@ app.post("/deposit", verifyIfExistAccountCPF, (request, response) => {
     return response.status(201).json(statementOperation);
 });
 
-app.post("/teste", verifyIfExistAccountCPF, (request, response) => {
-    const { teste } = request.body;
-    console.log(teste);
+app.post("/withdraw", verifyIfExistAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
 
-    return response.status(201).send();
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount){
+        return response.status(400).json({ error: "Insufficient funds" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).json(statementOperation);
 });
+
+app.get("/statement/date", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+    const { date } = request.query;
+
+    const formatDate = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter(
+        (statement) => 
+            statement.created_at.toDateString() === new Date(formatDate).toDateString());
+
+    return response.json(statement);
+});
+
+app.put("/account", verifyIfExistAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request
+
+    customer.name = name;
+
+    return response.status(200).json({name: customer.name});
+});
+
+app.get("/account", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request
+
+    return response.status(200).json(customer);
+});
+
+app.delete("/account", verifyIfExistAccountCPF, (request, reponse) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return reponse.status(200).json(customer);
+});
+
+app.get("/balance", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+    
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+})
+
+
+
 
 app.listen(3333, () => console.log("server started on port 3333"));
