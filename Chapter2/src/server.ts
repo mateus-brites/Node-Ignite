78073b6c1d85f9cb4dee51d65c16f7be81a8ead8
Mateus Brites <mateusmmo15@gmail.com>
2021-07-13 12:31:45 -0300

    Implementação da documentação

--- Chapter2/src/server.ts
+++ Chapter2/src/server.ts
@@ -1,11 +1,17 @@
 import express from "express";
 import { router } from "./routes";
+import SwaggerUI from "swagger-ui-express";
+
+import SwaggerFile from "./swagger.json"
+
 
 
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(SwaggerFile))
 
 app.use(router);
 
