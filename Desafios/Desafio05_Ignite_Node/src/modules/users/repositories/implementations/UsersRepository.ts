a1e595ae8c6dc8f6918c6070b5924a601c77bc55
Mateus Brites <mateusmmo15@gmail.com>
2021-07-18 21:51:56 -0300

    Fim do desafio 05

---
+++ Desafios/Desafio05_Ignite_Node/src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,47 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    const user = await this.repository.findOne(
+      user_id, 
+      { relations: ["games"] }
+    );
+
+    if (!user) {
+      throw new Error("User does not exist!");
+    }
+
+    return user;
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query(`
+    SELECT * 
+    FROM users
+    ORDER BY first_name ASC
+  `); 
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return await this.repository.query(`
+      SELECT * 
+      FROM users
+      WHERE LOWER(first_name) = LOWER('${first_name}') AND LOWER(last_name) = LOWER('${last_name}')
+    `); 
+  }
+}
