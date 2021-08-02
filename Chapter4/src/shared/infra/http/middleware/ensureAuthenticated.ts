0b51d2608700691e56c2e7bab3d73dfb5daabab7
Mateus Brites <mateusmmo15@gmail.com>
2021-08-01 23:35:37 -0300

    corrigindo o refresh token

--- Chapter4/src/shared/infra/http/middleware/ensureAuthenticated.ts
+++ Chapter4/src/shared/infra/http/middleware/ensureAuthenticated.ts
@@ -3,8 +3,6 @@ import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
 import auth from "@config/auth";
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
-import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 interface IPayload {
@@ -18,8 +16,6 @@ export async function ensureAuthenticated(
 ) {
     const authHeader = request.headers.authorization;
 
-    const usersTokensRepository = new UsersTokensRepository();
-
     if (!authHeader) {
         throw new AppError("Token is missing", 401);
     }
@@ -27,19 +23,7 @@ export async function ensureAuthenticated(
     const [, token] = authHeader.split(" ");
 
     try {
-        const { sub: user_id } = verify(
-            token,
-            auth.secret_refresh_token
-        ) as IPayload;
-
-        const user = await usersTokensRepository.findByUserIdAndRefreshToken(
-            user_id,
-            token
-        );
-
-        if (!user) {
-            throw new AppError("User does not exist", 401);
-        }
+        const { sub: user_id } = verify(token, auth.secret_token) as IPayload;
 
         request.user = {
             id: user_id,
