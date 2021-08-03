c98c84de9b73ab4acfb63e8531451297bba2c799
Mateus Brites <mateusmmo15@gmail.com>
2021-08-03 15:52:55 -0300

    Bug corrigido

--- Chapter4/src/modules/cars/useCase/createCategory/CreateCategoryController.spec.ts
+++ Chapter4/src/modules/cars/useCase/createCategory/CreateCategoryController.spec.ts
@@ -36,7 +36,7 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    const { refresh_token } = responseToken.body;
+    const { token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
@@ -45,7 +45,7 @@ describe("Create Category Controller", () => {
         description: "Category Supertest description",
       })
       .set({
-        Authorization: `Bearer ${refresh_token}`,
+        Authorization: `Bearer ${token}`,
       });
 
     expect(response.status).toBe(201);
@@ -57,7 +57,7 @@ describe("Create Category Controller", () => {
       password: "admin",
     });
 
-    const { refresh_token } = responseToken.body;
+    const { token } = responseToken.body;
 
     const response = await request(app)
       .post("/categories")
@@ -66,7 +66,7 @@ describe("Create Category Controller", () => {
         description: "Category Supertest description",
       })
       .set({
-        Authorization: `Bearer ${refresh_token}`,
+        Authorization: `Bearer ${token}`,
       });
 
     expect(response.status).toBe(400);
